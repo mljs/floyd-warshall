3d76484a0e440ec6a86f85ea8c17d3d92e94cfc6
maasencioh <maasencioh@gmail.com>
2016-08-30 15:34:32 +0200

    test: initial test to pass

--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const floydWarshall = require('..');
+const Matrix = require('ml-matrix');
 
-describe('floyd-warshall test', function () {
-
-    it('Something test', function () {
-
+describe('Floyd Warshall test', function () {
+    it('Directed graph', function () {
+        let adjMatrix = new Matrix([
+            [1, 1, 1, 0],
+            [1, 1, 0, 0],
+            [1, 0, 1, 0],
+            [0, 0, 0, 1]
+        ]);
+        let distMatrix = floydWarshall(adjMatrix);
+        distMatrix.to2DArray().should.be.eql([
+            [ 0,  1,  1, -1],
+            [ 1,  0,  2, -1],
+            [ 1,  2,  0, -1],
+            [-1, -1, -1,  0]
+        ]);
     });
 });
